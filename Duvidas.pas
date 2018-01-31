04092a22c380ee62004a65603d3367292fb81fce
gabrielthinassi <35746588+gabrielthinassi@users.noreply.github.com>
2018-01-31 01:12:25 -0200

    30-01-2018 Duvida
    Duvida Color

--- Duvidas.pas
+++ Duvidas.pas
@@ -6,6 +6,8 @@ implementation
 
 {
 1 - Porque não consigo acessar o SM no FrmServidor? (preciso acessar para alterar os Params da Conexão)
+
+2 - Porque não consigo alterar a Propriedade Color do Label no servidor Dinamicamente?
 }
 
 
